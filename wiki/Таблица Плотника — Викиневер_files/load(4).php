6f23745561863843fd12e72e1623b3a739d3959b
Koldun1986 <koldunchik1986@gmail.com>
2026-04-30 12:17:44 +0300

    Wiki таблицы

---
+++ wiki/Таблица Плотника — Викиневер_files/load(4).php
@@ -0,0 +1,36 @@
+var _____WB$wombat$assign$function_____=function(name){return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name))||self[name];};if(!self.__WB_pmw){self.__WB_pmw=function(obj){this.__WB_source=obj;return this;}}{
+let window = _____WB$wombat$assign$function_____("window");
+let self = _____WB$wombat$assign$function_____("self");
+let document = _____WB$wombat$assign$function_____("document");
+let location = _____WB$wombat$assign$function_____("location");
+let top = _____WB$wombat$assign$function_____("top");
+let parent = _____WB$wombat$assign$function_____("parent");
+let frames = _____WB$wombat$assign$function_____("frames");
+let opens = _____WB$wombat$assign$function_____("opens");
+jQuery(function($){$('div.vectorMenu').each(function(){var self=this;$('h5:first a:first',this).click(function(e){$('.menu:first',self).toggleClass('menuForceShow');e.preventDefault();}).focus(function(){$(self).addClass('vectorMenuFocus');}).blur(function(){$(self).removeClass('vectorMenuFocus');});});});;mw.loader.state({"skins.vector":"ready"});
+
+/* cache key: mediawiki-wiki_:resourceloader:filter:minify-js:7:0fcba82177db6429d02096ea1f0465ed */
+
+}
+
+/*
+     FILE ARCHIVED ON 14:01:20 Dec 31, 2023 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 07:24:45 Apr 29, 2026.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 0.627
+  exclusion.robots: 0.062
+  exclusion.robots.policy: 0.052
+  esindex: 0.011
+  cdx.remote: 29.106
+  LoadShardBlock: 269.26 (3)
+  PetaboxLoader3.datanode: 151.384 (5)
+  PetaboxLoader3.resolve: 191.502 (2)
+  load_resource: 136.777
+  loaddict: 66.776
+*/
---
+++ wiki/Таблица чертежей вещей — Викиневер_files/load(4).php
@@ -0,0 +1,31 @@
+var _____WB$wombat$assign$function_____=function(name){return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name))||self[name];};if(!self.__WB_pmw){self.__WB_pmw=function(obj){this.__WB_source=obj;return this;}}{
+let window = _____WB$wombat$assign$function_____("window");
+let self = _____WB$wombat$assign$function_____("self");
+let document = _____WB$wombat$assign$function_____("document");
+let location = _____WB$wombat$assign$function_____("location");
+let top = _____WB$wombat$assign$function_____("top");
+let parent = _____WB$wombat$assign$function_____("parent");
+let frames = _____WB$wombat$assign$function_____("frames");
+let opens = _____WB$wombat$assign$function_____("opens");
+jQuery(function($){$('div.vectorMenu').each(function(){var self=this;$('h5:first a:first',this).click(function(e){$('.menu:first',self).toggleClass('menuForceShow');e.preventDefault();}).focus(function(){$(self).addClass('vectorMenuFocus');}).blur(function(){$(self).removeClass('vectorMenuFocus');});});});;mw.loader.state({"skins.vector":"ready"});
+
+/* cache key: mediawiki-wiki_:resourceloader:filter:minify-js:7:0fcba82177db6429d02096ea1f0465ed */
+
+}
+
+/*
+     FILE ARCHIVED ON 14:01:20 Dec 31, 2023 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 07:13:46 Apr 29, 2026.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  capture_cache.get: 0.643
+  load_resource: 127.225
+  PetaboxLoader3.resolve: 41.179
+  PetaboxLoader3.datanode: 70.643 (2)
+  loaddict: 46.961
+*/
