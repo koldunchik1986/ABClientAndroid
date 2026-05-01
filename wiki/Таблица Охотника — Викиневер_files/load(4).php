22dd48b4b93d43256f15298551d68ca2c7fcb03a
Koldun1986 <koldunchik1986@gmail.com>
2026-05-01 14:12:28 +0300

    wiki

---
+++ wiki/Таблица Охотника — Викиневер_files/load(4).php
@@ -0,0 +1,31 @@
+var _____WB$wombat$assign$function_____=function(name){return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name))||self[name];};if(!self.__WB_pmw){self.__WB_pmw=function(obj){this.__WB_source=obj;return this;}}{
+let window = _____WB$wombat$assign$function_____("window");
+let self = _____WB$wombat$assign$function_____("self");
+let document = _____WB$wombat$assign$function_____("document");
+let location = _____WB$wombat$assign$function_____("location");
+let top = _____WB$wombat$assign$function_____("top");
+let parent = _____WB$wombat$assign$function_____("parent");
+let frames = _____WB$wombat$assign$function_____("frames");
+let opens = _____WB$wombat$assign$function_____("opens");
+jQuery(function($){$('div.vectorMenu').each(function(){var self=this;$('h5:first a:first',this).click(function(e){$('.menu:first',self).toggleClass('menuForceShow');e.preventDefault();}).focus(function(){$(self).addClass('vectorMenuFocus');}).blur(function(){$(self).removeClass('vectorMenuFocus');});});});;mw.loader.state({"skins.vector":"ready"});
+
+/* cache key: mediawiki-wiki_:resourceloader:filter:minify-js:7:0fcba82177db6429d02096ea1f0465ed */
+
+}
+
+/*
+     FILE ARCHIVED ON 14:01:20 Dec 31, 2023 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 10:53:05 May 01, 2026.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  capture_cache.get: 0.421
+  load_resource: 304.614
+  PetaboxLoader3.resolve: 247.128
+  PetaboxLoader3.datanode: 46.376 (2)
+  loaddict: 30.195
+*/
